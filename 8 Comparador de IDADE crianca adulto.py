1f1f61ce6857d3f5ee95b66be47b615e35abe488
Thiago Yugue Kono Campos <106500654+kono100@users.noreply.github.com>
2023-03-10 04:11:32 -0300

    Add files via upload

---
+++ 8 Comparador de IDADE crianca adulto.py
@@ -0,0 +1,13 @@
+a = input("Digite a idade da pessoa: ")
+idade = int (a)
+
+if idade <=12:  
+        print ("Criança\n")
+elif idade <=18:
+    print ("Adolescente\n")
+elif idade <=59:
+    print ("Adulto\n")
+elif idade <=200:
+    print ("Terceira Idade\n")
+elif idade <=10000:
+    print ("é alienígena ?\n")
